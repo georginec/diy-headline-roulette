7a05e75a37fbf189bfed97653325ef8d847bda73
georginec <georgine@uow.edu.au>
2017-04-19 13:35:59 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ $(function(){
 
     // YOU MUST EDIT THIS SECTION
     // You must supply a Trove API key
-    var troveAPIKey = '';
+    var troveAPIKey = 'ifcltsf45qbotvkh';
     // Either provide full API query here or include options below
     var apiQuery = '';
     // Words you want to search for -- separate multiple values with spaces, eg:
     // var keywords = 'weather wragge';
-    var keywords = '';
+    var keywords = 'overlanding overlander';
     // How you want to combine keywords -- all, any, or phrase
     var keywordType = 'all'
     // Newspaper id numbers -- separate multiple values with spaces, eg:
